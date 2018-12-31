b51de00e58c9f739bd09d2fbea6f52c65355758f
Nelson LaQuet <nelsonlaquet@gmail.com>
2018-12-30 23:57:27 -0600

    Basic support for changing the speed of an apng emote.

--- js/berrymotes.core.js
+++ js/berrymotes.core.js
@@ -66,9 +66,9 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
         });
     };
 
-
     Bem.effectStack = [];
-    Bem.emoteRegex = /\[([^\]]*)\]\(\/([\w:!#\/]+)([-\w!]*)([^)]*)\)/gi;
+    Bem.emoteRegex = /\[([^\]]*)\]\(\/([\w:!#\/]+)([-\w!.]*)([^)]*)\)/gi;
+    Bem.apngSpeedRegex = /a(\d*(\.\d+)?)/;
     Bem.emRegex = /\*([^\]\*]+)\*/gi;
     Bem.strongRegex = /\*\*([^\]\*]+)\*\*/gi;
     Bem.searchPage = 0;
@@ -195,10 +195,10 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
                     if (Bem.isEmoteEligible(emote)) {
                         var innerText = Bem.formatInnerText(match[1]);
                         emoteLocations.push({
-                                start: match.index,
-                                replace_length: match[0].length,
-                                emote_html: Bem.getEmoteHtml(emote, match[3], innerText, match[4])
-                            }
+                            start: match.index,
+                            replace_length: match[0].length,
+                            emote_html: Bem.getEmoteHtml(emote, match[3], innerText, match[4])
+                        }
                         );
                     }
                 }
@@ -220,7 +220,7 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
         var emoteHtml = ['<span class="berryemote',
             emote.height > Bem.maxEmoteHeight ? ' resize' : '',
             Bem.apngSupported == false && emote.apng_url ? ' canvasapng' : '',
-            Bem.onlyHover == true || ( emote.nsfw && Bem.onlyHoverNSFW ) ? ' berryemote_hover' : '',
+            Bem.onlyHover == true || (emote.nsfw && Bem.onlyHoverNSFW) ? ' berryemote_hover' : '',
             '" ',
             'style="',
             'height:', emote.height, 'px; ',
@@ -228,7 +228,7 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
             'display:inline-block; ',
             'position: relative; overflow: hidden;', '" ',
             'flags="', flags, '" ',
-            'emote_id="', emote.id , '">', innerText || "" , '</span>'
+            'emote_id="', emote.id, '">', innerText || "", '</span>'
 
         ];
         if (altText) {
@@ -244,7 +244,7 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
                 eligible = false;
             }
             // allow subreddit blacklisting
-            if ('/r/'+emote.sr == $.trim(Bem.blacklist[j])) {
+            if ('/r/' + emote.sr == $.trim(Bem.blacklist[j])) {
                 eligible = false;
             }
         }
@@ -271,7 +271,8 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
             'height': Math.ceil(height - offset),
             'display': 'inline-block',
             'margin-top': offset,
-            'position': 'relative'});
+            'position': 'relative'
+        });
     };
 
     Bem.postEmoteEffects = function (message, isSearch, ttl, username) {
@@ -339,6 +340,11 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
         }
 
         var emotes = message.find('.berryemote');
+
+        const emotePostprocess = new Array(emotes.length);
+        for (let i = 0; i < emotes.length; i++)
+            emotePostprocess[i] = {};
+
         if (!isSearch && Bem.effects) {
             $.each(emotes, function (index, emoteDom) {
                 var $emote = $(emoteDom);
@@ -356,6 +362,8 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
                 var spin;
                 var brody;
                 var needsWrapper;
+                const postprocess = emotePostprocess[index];
+
                 for (var i = 0; i < flags.length; ++i) {
                     if (Bem.animationSpeeds.indexOf(flags[i]) > -1 || flags[i].match(/^s\d/)) {
                         speed = flags[i];
@@ -374,7 +382,15 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
                         (Bem.enableBrody && flags[i] == 'brody')) {
                         needsWrapper = true;
                     }
+
+                    const apngMatch = flags[i].match(Bem.apngSpeedRegex);
+                    if (apngMatch) {
+                        var matchedSpeed = parseFloat(apngMatch[1]);
+                        if (!isNaN(matchedSpeed))
+                            postprocess.speed = matchedSpeed;
+                    }
                 }
+
                 for (var i = 0; i < flags.length; ++i) {
                     if (Bem.enableSpin && Bem.spinAnimations.indexOf(flags[i]) != -1) {
                         animations.push(flags[i] + ' 2s infinite linear');
@@ -451,21 +467,20 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
                 }
 
                 if (animations.length > 0) {
-                    if (ttl) {
-                        Bem.effectStack.push({"ttl": ttl, "$emote": $emote});
-                    }
+                    if (ttl)
+                        Bem.effectStack.push({ "ttl": ttl, "$emote": $emote });
 
-                    //$emote.css('will-change', 'transform');
                     $emote.css('animation', animations.join(',').replace('!', '-'));
                 }
 
-                if (needsWrapper) {
+                if (needsWrapper)
                     $emote.parent().css('animation', wrapperAnimations.join(',').replace('!', '-'));
-                }
-                if (Bem.enableReverse && reverse) transforms.push('scaleX(-1)');
-                if (transforms.length > 0) {
+
+                if (Bem.enableReverse && reverse)
+                    transforms.push('scaleX(-1)');
+
+                if (transforms.length > 0)
                     $emote.css('transform', transforms.join(' '));
-                }
             });
         }
         $.each(emotes, function (index, emoteDom) {
@@ -489,39 +504,53 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
             }
             $emote.css('background-position', position_string);
             if ($emote.is('.canvasapng') == false) {
-                const bgImage = new Image();
-                bgImage.onload = function(){
-                    $emote.css('background-image', ['url(', bgImage.src, ')'].join(''));
-                    if (Bem.onEmoteLoad) {
-                        Bem.onEmoteLoad();
-                    }
-                };
-                bgImage.src = emote['background-image'] || emote['apng_url'];
+                const postSettings = emotePostprocess[index];
+                if (Object.keys(postSettings).length) {
+                    postProcessEmote(emote, postSettings)
+                        .then(
+                            function ok(dataUrl) {
+                                $emote.css("background-image", dataUrl);
+                            },
+                            function notOk(e) {
+                                console.error(`Could not do post-processing for emote ${emote.names.join(", ")}!`);
+                                console.error(e);
+                                $emote[0].style.background = `url(${emote['background-image'] || emote['apng_url']})`;
+                            });
+                } else {
+                    const bgImage = new Image();
+                    bgImage.onload = function () {
+                        $emote.css('background-image', ['url(', bgImage.src, ')'].join(''));
+                        if (Bem.onEmoteLoad) {
+                            Bem.onEmoteLoad();
+                        }
+                    };
+                    bgImage.src = emote['background-image'] || emote['apng_url'];
+                }
             }
             var flags = $emote.attr('flags');
 
             $emote.attr('title', [emote.names,
                 ' from /r/',
-                emote.sr,
-                flags ? ' effects: ' + flags : ''].join(''));
+            emote.sr,
+            flags ? ' effects: ' + flags : ''].join(''));
 
             if (emote['hover-background-position'] || emote['hover-background-image']) {
                 $emote.hover(function () {
-                        var $this = $(this);
-                        var positionString = (emote['hover-background-position'] || ['0px', '0px']).join(' ');
-                        var width = emote['hover-width'];
-                        var height = emote['hover-height'];
-                        $this.css('background-position', positionString);
-                        if (emote['hover-background-image']) {
-                            $this.css('background-image', ['url(', emote['hover-background-image'], ')'].join(''));
-                        }
-                        if (width) {
-                            $this.css('width', width);
-                        }
-                        if (height) {
-                            $this.css('height', height);
-                        }
-                    },
+                    var $this = $(this);
+                    var positionString = (emote['hover-background-position'] || ['0px', '0px']).join(' ');
+                    var width = emote['hover-width'];
+                    var height = emote['hover-height'];
+                    $this.css('background-position', positionString);
+                    if (emote['hover-background-image']) {
+                        $this.css('background-image', ['url(', emote['hover-background-image'], ')'].join(''));
+                    }
+                    if (width) {
+                        $this.css('width', width);
+                    }
+                    if (height) {
+                        $this.css('height', height);
+                    }
+                },
                     function () {
                         var $this = $(this);
                         var position_string = (emote['background-position'] || ['0px', '0px']).join(' ');
@@ -755,15 +784,15 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
                     if (bit.match(srRegex)) {
                         var trim = bit.match(srRegex)[0].length;
                         if (bit[0] == '-' || bit[0] == '+') {
-                            srs.push({match: bit[0] != '-', sdr: sdrify(bit.substring(trim))});
+                            srs.push({ match: bit[0] != '-', sdr: sdrify(bit.substring(trim)) });
                         } else {
-                            srs.push({match: true, sdr: sdrify(bit.substring(trim))});
+                            srs.push({ match: true, sdr: sdrify(bit.substring(trim)) });
                         }
                     } else if (bit.match(tagRegex)) {
                         var trim = bit.match(tagRegex)[0].length;
                         var tag = bit.substring(trim);
                         var tagRegex = tag in Bem.tagRegexes ? sdrify(Bem.tagRegexes[tag]) : sdrify(tag);
-                        tags.push({match: bit[0] != '-', sdr: tagRegex});
+                        tags.push({ match: bit[0] != '-', sdr: tagRegex });
                     } else {
                         terms.push({
                             any: new RegExp(bit, 'i'),
@@ -1040,4 +1069,209 @@ Bem.cdn_origin = Bem.cdn_origin || 'https://cdn.berrytube.tv/berrymotes';
         } else if (Bem.debug) console.log("Load is a negative, going quiet.");
     });
 
+    async function postProcessEmote(emote, postprocess) {
+        const response = await fetch(emote["apng_url"] || emote["background-image"]);
+        if (!response.ok)
+            throw new Error(`Bad response: ${response.status}`);
+
+        let buffer = await response.arrayBuffer();
+
+        if (emote.hasOwnProperty("apng_url")) {
+            if (postprocess.hasOwnProperty("speed")) {
+                try {
+                    buffer = setApngSpeed(buffer, postprocess.speed);
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        }
+
+        return `url(data:image/png;base64,${base64ArrayBuffer(buffer)})`;
+    }
+
+    const apngHeader = [137, 80, 78, 71, 13, 10, 26, 10]
+    function setApngSpeed(buffer, speed) {
+        if (speed == 0) {
+            return buffer;
+        }
+
+        speed = 1 / speed;
+
+        const view = new Uint8Array(buffer);
+        const data = new DataView(buffer);
+
+        for (let i = 0; i < apngHeader.length; i++) {
+            if (view[i] != apngHeader[i])
+                throw new Error(`Unexpected header near byte ${i}. ${view[i]} != ${apngHeader[i]}`);
+        }
+
+        parseChunks(view, (type, _, offset, length) => {
+            if (type != "fcTL")
+                return;
+
+            // read about this frame here https://wiki.mozilla.org/APNG_Specification#.60fcTL.60:_The_Frame_Control_Chunk
+            const delayNumerator = readWord(view, offset + 8 + 20) || 1;
+            const delayDenomerator = readWord(view, offset + 8 + 22) || 1;
+
+            const newDelay = Math.round((delayNumerator / delayDenomerator) * speed * 1000) / 1000;
+            const [newNumerator, newDenominator] = convertDecimalToFraction(newDelay);
+
+            data.setUint16(offset + 8 + 20, newNumerator);
+            data.setUint16(offset + 8 + 22, newDenominator);
+
+            // don't forget to re-caulate the CRC! Firefox doesn't seem to care, but Chrome will not render the apng at
+            // all if any frames have a wrong checksum.
+            const newCrc = Bem.Crc32(view.slice(offset + 4, offset + 4 + 4 + length));
+            data.setUint32(offset + length + 4 + 4, newCrc);
+        });
+
+        return buffer;
+    }
+
+    // for performance
+    // taken from https://gist.github.com/jonleighton/958841
+    function base64ArrayBuffer(arrayBuffer) {
+        var base64 = ''
+        var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+        var bytes = new Uint8Array(arrayBuffer)
+        var byteLength = bytes.byteLength
+        var byteRemainder = byteLength % 3
+        var mainLength = byteLength - byteRemainder
+
+        var a, b, c, d
+        var chunk
+
+        // Main loop deals with bytes in chunks of 3
+        for (var i = 0; i < mainLength; i = i + 3) {
+            // Combine the three bytes into a single integer
+            chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+            // Use bitmasks to extract 6-bit segments from the triplet
+            a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+            b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
+            c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
+            d = chunk & 63               // 63       = 2^6 - 1
+
+            // Convert the raw binary segments to the appropriate ASCII encoding
+            base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+        }
+
+        // Deal with the remaining bytes and padding
+        if (byteRemainder == 1) {
+            chunk = bytes[mainLength]
+
+            a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+            // Set the 4 least significant bits to zero
+            b = (chunk & 3) << 4 // 3   = 2^2 - 1
+
+            base64 += encodings[a] + encodings[b] + '=='
+        } else if (byteRemainder == 2) {
+            chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+            a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+            b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+
+            // Set the 2 least significant bits to zero
+            c = (chunk & 15) << 2 // 15    = 2^4 - 1
+
+            base64 += encodings[a] + encodings[b] + encodings[c] + '='
+        }
+
+        return base64
+    }
+
+    function getGreatedCommongDenom(a, b) {
+        if (b < 0.0000001)
+            return a;
+        return getGreatedCommongDenom(b, Math.floor(a % b));
+    }
+
+    function convertDecimalToFraction(decimal) {
+        const len = decimal.toString().length - 2;
+        const denominator = Math.pow(10, len);
+        const numerator = decimal * denominator;
+        const divisor = getGreatedCommongDenom(numerator, denominator)
+        return [numerator / divisor, denominator / divisor];
+    }
+
+    // the following helper functions are taken from
+    // https://github.com/davidmz/apng-canvas/blob/master/src/parser.js
+
+    /**
+     * @param {Uint8Array} bytes
+     * @param {function(string, Uint8Array, int, int)} callback
+     */
+    function parseChunks(bytes, callback) {
+        var off = 8;
+        do {
+            var length = readDWord(bytes, off);
+            var type = readString(bytes, off + 4, 4);
+            var res = callback(type, bytes, off, length);
+            off += 12 + length;
+        } while (res !== false && type != "IEND" && off < bytes.length);
+    }
+
+    /**
+     * @param {Uint8Array} bytes
+     * @param {int} off
+     * @return {int}
+     */
+    function readDWord(bytes, off) {
+        var x = 0;
+        // Force the most-significant byte to unsigned.
+        x += ((bytes[0 + off] << 24) >>> 0);
+        for (var i = 1; i < 4; i++) x += ((bytes[i + off] << ((3 - i) * 8)));
+        return x;
+    }
+
+    /**
+     * @param {Uint8Array} bytes
+     * @param {int} off
+     * @return {int}
+     */
+    function readWord(bytes, off) {
+        var x = 0;
+        for (var i = 0; i < 2; i++) x += (bytes[i + off] << ((1 - i) * 8));
+        return x;
+    }
+
+    function readString(bytes, off, length) {
+        var chars = Array.prototype.slice.call(bytes.subarray(off, off + length));
+        return String.fromCharCode.apply(String, chars);
+    }
+
+    // taken from https://stackoverflow.com/a/18639903
+    Bem.Crc32 = (function () {
+        var table = new Uint32Array(256);
+
+        // Pre-generate crc32 polynomial lookup table
+        // http://wiki.osdev.org/CRC32#Building_the_Lookup_Table
+        // ... Actually use Alex's because it generates the correct bit order
+        //     so no need for the reversal function
+        for (var i = 256; i--;) {
+            var tmp = i;
+
+            for (var k = 8; k--;) {
+                tmp = tmp & 1 ? 3988292384 ^ tmp >>> 1 : tmp >>> 1;
+            }
+
+            table[i] = tmp;
+        }
+
+        // crc32b
+        // Example input        : [97, 98, 99, 100, 101] (Uint8Array)
+        // Example output       : 2240272485 (Uint32)
+        return function (data) {
+            var crc = -1; // Begin with all bits set ( 0xffffffff )
+
+            for (var i = 0, l = data.length; i < l; i++) {
+                crc = crc >>> 8 ^ table[crc & 255 ^ data[i]];
+            }
+
+            return (crc ^ -1) >>> 0; // Apply binary NOT
+        };
+
+    })();
 })(Bem.jQuery);
