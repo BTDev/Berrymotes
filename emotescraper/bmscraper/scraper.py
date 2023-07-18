38fb43b1d4af4e3f70d27a9f20b447e957bf6723
Atte Virtanen <aten.email@gmail.com>
2023-07-18 22:15:04 +0300

    Make scraper look like a browser

--- emotescraper/bmscraper/scraper.py
+++ emotescraper/bmscraper/scraper.py
@@ -50,7 +50,20 @@ class BMScraper(FileNameUtils):
         self.mutex = threading.RLock()
 
         self._requests = requests.Session()
-        self._requests.headers = {'user-agent', 'User-Agent: Ponymote harvester v2.0 by /u/marminatoror'}
+        # self._requests.headers = {'user-agent', 'User-Agent: Ponymote harvester v2.0 by /u/marminatoror'}
+        self._requests.headers.update({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Connection": "keep-alive",
+            "TE": "trailers",
+        })
 
     def _dedupe_emotes(self):
         with self.mutex:
